6ab80996b81455b2e129f01cd2b978f288a29827
Cemile Karabulut <cemile.karabulut@metu.edu.tr>
2022-03-23 23:20:22 +0300

    Initial commit

---
+++ Thread Race/src/EvenOddDetectionThread.java
@@ -0,0 +1,55 @@
+import java.util.Collections;
+import java.util.List;
+
+public class EvenOddDetectionThread implements Runnable {
+    private List<Integer> list;
+    private List<Integer> evenNumbers;
+    private List<Integer> oddNumbers;
+
+    public EvenOddDetectionThread(List<Integer> list, List<Integer> evenNumbers, List<Integer> oddNumbers) {
+        this.list = list;
+        this.evenNumbers = evenNumbers;
+        this.oddNumbers = oddNumbers;
+    }
+
+    @Override
+    public void run() {
+        if (Thread.currentThread().getName().substring(7).equals("0")) {
+            for (int i = 0; i < 2500; i++) {
+                if (this.list.get(i) % 2 == 0)
+                    evenNumbers.add(this.list.get(i));
+                else
+                    oddNumbers.add(this.list.get(i));
+            }
+            System.out.println("Even numbers in "+Thread.currentThread().getName()+": " + evenNumbers.size()+ " Max even number is " + Collections.max(evenNumbers));
+            System.out.println("Odd numbers in "+Thread.currentThread().getName()+": " + oddNumbers.size()+ " Max odd number is " + Collections.max(oddNumbers));
+        } else if (Thread.currentThread().getName().substring(7).equals("1")) {
+            for (int i = 2500; i < 5000; i++) {
+                if (this.list.get(i) % 2 == 0)
+                    evenNumbers.add(this.list.get(i));
+                else
+                    oddNumbers.add(this.list.get(i));
+            }
+            System.out.println("Even numbers in "+Thread.currentThread().getName()+": " + evenNumbers.size()+ " Max even number is " + Collections.max(evenNumbers));
+            System.out.println("Odd numbers in "+Thread.currentThread().getName()+": " + oddNumbers.size()+ " Max odd number is " + Collections.max(oddNumbers));
+        } else if (Thread.currentThread().getName().substring(7).equals("2")) {
+            for (int i = 5000; i < 7500; i++) {
+                if (this.list.get(i) % 2 == 0)
+                    evenNumbers.add(this.list.get(i));
+                else
+                    oddNumbers.add(this.list.get(i));
+            }
+            System.out.println("Even numbers in "+Thread.currentThread().getName()+": " + evenNumbers.size()+ " Max even number is " + Collections.max(evenNumbers));
+            System.out.println("Odd numbers in "+Thread.currentThread().getName()+": " + oddNumbers.size()+ " Max odd number is " + Collections.max(oddNumbers));
+        } else if (Thread.currentThread().getName().substring(7).equals("3")) {
+            for (int i = 7500; i < 10000; i++) {
+                if (this.list.get(i) % 2 == 0)
+                    evenNumbers.add(this.list.get(i));
+                else
+                    oddNumbers.add(this.list.get(i));
+            }
+            System.out.println("Even numbers in "+Thread.currentThread().getName()+": " + evenNumbers.size()+ " Max even number is " + Collections.max(evenNumbers));
+            System.out.println("Odd numbers in "+Thread.currentThread().getName()+": " + oddNumbers.size()+ " Max odd number is " + Collections.max(oddNumbers));
+        }
+    }
+}
---
+++ Thread Race/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String... args) throws InterruptedException {
+        List<Integer> numList = new ArrayList<>();
+        for(int i = 1; i <= 10000; i++){
+            numList.add(i);
+        }
+
+        List<Integer> evenNumbers = new ArrayList<>();
+        List<Integer> oddNumbers = new ArrayList<>();
+
+        EvenOddDetectionThread detector = new EvenOddDetectionThread(numList,evenNumbers,oddNumbers);
+
+        Thread firstThread = new Thread(detector);
+        Thread secondThread = new Thread(detector);
+        Thread thirdThread = new Thread(detector);
+        Thread fourthThread = new Thread(detector);
+
+        firstThread.start();
+        secondThread.start();
+        thirdThread.start();
+        fourthThread.start();
+
+        firstThread.join();
+        secondThread.join();
+        thirdThread.join();
+        fourthThread.join();
+
+        System.out.println("Even numbers in total: " + evenNumbers.size()+ " Max even number is " + Collections.max(evenNumbers));
+        System.out.println("Odd numbers in total: " + oddNumbers.size()+ " Max odd number is " + Collections.max(oddNumbers));
+    }
+}
