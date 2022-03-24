170e41cf2ca26ceb4ce93fa7f570a068e4814e46
Cemile Karabulut <cemile.karabulut@metu.edu.tr>
2022-03-24 22:48:58 +0300

    Threadde iyileştirme

--- Thread Race/src/EvenOddDetectionThread.java
+++ Thread Race/src/EvenOddDetectionThread.java
@@ -1,4 +1,3 @@
-import java.util.Collections;
 import java.util.List;
 
 public class EvenOddDetectionThread implements Runnable {
@@ -13,43 +12,14 @@ public class EvenOddDetectionThread implements Runnable {
     }
 
     @Override
-    public void run() {
-        if (Thread.currentThread().getName().substring(7).equals("0")) {
-            for (int i = 0; i < 2500; i++) {
-                if (this.list.get(i) % 2 == 0)
-                    evenNumbers.add(this.list.get(i));
+    public synchronized void run() {
+            for (Integer num : list) {
+                if (num % 2 == 0)
+                    evenNumbers.add(num);
                 else
-                    oddNumbers.add(this.list.get(i));
+                    oddNumbers.add(num);
             }
-            System.out.println("Even numbers in " + Thread.currentThread().getName() + ": " + evenNumbers.size() + " Max even number is " + Collections.max(evenNumbers));
-            System.out.println("Odd numbers in " + Thread.currentThread().getName() + ": " + oddNumbers.size() + " Max odd number is " + Collections.max(oddNumbers));
-        } else if (Thread.currentThread().getName().substring(7).equals("1")) {
-            for (int i = 2500; i < 5000; i++) {
-                if (this.list.get(i) % 2 == 0)
-                    evenNumbers.add(this.list.get(i));
-                else
-                    oddNumbers.add(this.list.get(i));
-            }
-            System.out.println("Even numbers in " + Thread.currentThread().getName() + ": " + evenNumbers.size() + " Max even number is " + Collections.max(evenNumbers));
-            System.out.println("Odd numbers in " + Thread.currentThread().getName() + ": " + oddNumbers.size() + " Max odd number is " + Collections.max(oddNumbers));
-        } else if (Thread.currentThread().getName().substring(7).equals("2")) {
-            for (int i = 5000; i < 7500; i++) {
-                if (this.list.get(i) % 2 == 0)
-                    evenNumbers.add(this.list.get(i));
-                else
-                    oddNumbers.add(this.list.get(i));
-            }
-            System.out.println("Even numbers in " + Thread.currentThread().getName() + ": " + evenNumbers.size() + " Max even number is " + Collections.max(evenNumbers));
-            System.out.println("Odd numbers in " + Thread.currentThread().getName() + ": " + oddNumbers.size() + " Max odd number is " + Collections.max(oddNumbers));
-        } else if (Thread.currentThread().getName().substring(7).equals("3")) {
-            for (int i = 7500; i < 10000; i++) {
-                if (this.list.get(i) % 2 == 0)
-                    evenNumbers.add(this.list.get(i));
-                else
-                    oddNumbers.add(this.list.get(i));
-            }
-            System.out.println("Even numbers in " + Thread.currentThread().getName() + ": " + evenNumbers.size() + " Max even number is " + Collections.max(evenNumbers));
-            System.out.println("Odd numbers in " + Thread.currentThread().getName() + ": " + oddNumbers.size() + " Max odd number is " + Collections.max(oddNumbers));
-        }
+            System.out.println("Even numbers in " + Thread.currentThread().getName() + " Even numbers:  " + evenNumbers.toString());
+            System.out.println("Odd numbers in " + Thread.currentThread().getName() + " Odd numbers " + oddNumbers.toString());
     }
 }
--- Thread Race/src/Main.java
+++ Thread Race/src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Main {
     public static void main(String... args) throws InterruptedException {
@@ -9,15 +10,18 @@ public class Main {
             numList.add(i);
         }
 
-        List<Integer> evenNumbers = new ArrayList<>();
-        List<Integer> oddNumbers = new ArrayList<>();
+        List<Integer> evenNumbers = new CopyOnWriteArrayList<>();
+        List<Integer> oddNumbers = new CopyOnWriteArrayList<>();
 
-        EvenOddDetectionThread detector = new EvenOddDetectionThread(numList, evenNumbers, oddNumbers);
+        EvenOddDetectionThread detector1 = new EvenOddDetectionThread(new ArrayList<>(numList.subList(0,2500)), evenNumbers, oddNumbers);
+        EvenOddDetectionThread detector2 = new EvenOddDetectionThread(new ArrayList<>(numList.subList(2500,5000)), evenNumbers, oddNumbers);
+        EvenOddDetectionThread detector3 = new EvenOddDetectionThread(new ArrayList<>(numList.subList(5000,7500)), evenNumbers, oddNumbers);
+        EvenOddDetectionThread detector4 = new EvenOddDetectionThread(new ArrayList<>(numList.subList(7500,10000)), evenNumbers, oddNumbers);
 
-        Thread firstThread = new Thread(detector);
-        Thread secondThread = new Thread(detector);
-        Thread thirdThread = new Thread(detector);
-        Thread fourthThread = new Thread(detector);
+        Thread firstThread = new Thread(detector1);
+        Thread secondThread = new Thread(detector2);
+        Thread thirdThread = new Thread(detector3);
+        Thread fourthThread = new Thread(detector4);
 
         firstThread.start();
         secondThread.start();
@@ -29,7 +33,7 @@ public class Main {
         thirdThread.join();
         fourthThread.join();
 
-        System.out.println("Even numbers in total: " + evenNumbers.size() + " Max even number is " + Collections.max(evenNumbers));
-        System.out.println("Odd numbers in total: " + oddNumbers.size() + " Max odd number is " + Collections.max(oddNumbers));
+        System.out.println("Even numbers in total: " + evenNumbers.toString());
+        System.out.println("Odd numbers in total: " + oddNumbers.toString());
     }
 }
